29454e4b77c733e8809e84a3074ef2fe2a6bd975
Aleksi Partanen <aleksi.partanenn17@gmail.com>
2020-04-27 11:36:15 +0300

    Playing aroudn with circles and its variables

--- canvas.js
+++ canvas.js
@@ -41,30 +41,53 @@ var c = canvas.getContext('2d')
 // 	a.push(o)
 // }
 
+window.addEventListener('mousemove', (e) => {
+	mouse.x = e.x
+	mouse.y = e.y
+	//console.log(mouse)
+})
+
+window.addEventListener('resize', (e) => {
+	canvas.width = window.innerWidth
+	canvas.height = window.innerHeight
+})
 
 var mouse = {
 	x: undefined,
 	y: undefined
 }
 
-window.addEventListener('mousemove', (e) => {
-	mouse.x = e.x
-	mouse.y = e.y
-	//console.log(mouse)
-})
+// Settings/conf for circles
+let circlesAmount = 1000
+let maxRadius 	  = 60
+let minRadius 	  = 5
+let size 		  = 10
+let speed 		  = 3
+let growSpeed     =	5 // How fast circles change size
+
+// Color array for circles
+var colorArray = [
+	'#9C4AFF',
+	'#8C43E6',
+	'#7638C2',
+	'#5E2C99',
+	'#492378'
+]
+
 function Circle(x, y, dx, dy, radius) {
-	this.x 		= x		// X coordinate
-	this.y 		= y		// Y coordinate
-	this.dx 	= dx	// X velocity
-	this.dy 	= dy	// Y velocity
-	this.radius = radius	// Radius
+	this.x 			= x			// X coordinate
+	this.y 			= y			// Y coordinate
+	this.dx 		= dx		// X velocity
+	this.dy 		= dy		// Y velocity
+	this.radius 	= radius	// Radius
+	this.minRadius 	= radius	// Minimum radius, the size it has been created with
+	this.color  	= colorArray[Math.floor(Math.random() * colorArray.length)]
 
 	// Draws 
 	this.draw = () => {
 		c.beginPath()
 		c.arc(this.x, this.y, this.radius, Math.PI * 2, false)
-		c.strokeStyle = 'blue'
-		c.stroke()
+		c.fillStyle = this.color
 		c.fill()
 	}
 
@@ -84,17 +107,27 @@ function Circle(x, y, dx, dy, radius) {
 		
 		// If circle is closer than 50 pixels of mouse then
 		// grow its radius by 1 pixel to max of 100
-		if(mouse.x - this.x < 50 && mouse.x - this.x > -50 &&
+		if(mouse.x - this.x < 50 && mouse.x - this.x > -50 && 
 		   mouse.y - this.y < 50 && mouse.y - this.y > -50) {
-			if(this.radius < 100) {
-				this.radius += 1
+			// If radius size is not going to be more than maxRadius,
+			// then add growSpeed value to radius.
+			// Else set radius to maxRadius
+			if(this.radius+growSpeed < maxRadius) {
+				this.radius += growSpeed
+			} else {
+				this.radius = maxRadius
 			}
 		} 
 		// If circle goes farther than 50 pixels from mouse
 		// then shrink it by 1 pixel back to 30 pixel radius
 		else {
-			if(this.radius > 30) {
-				this.radius -= 1
+			// If radius size is not going to be less than minRadius,
+			// then reduse radius by growSpeed value.
+			// Else set radius to be the original radius
+			if(this.radius-growSpeed > this.minRadius) {
+				this.radius -= growSpeed
+			} else {
+				this.radius = this.minRadius
 			}
 		}
 		
@@ -108,14 +141,14 @@ function Circle(x, y, dx, dy, radius) {
 var circles = []
 
 // Create 100 circles with random velocities and coordinates
-for(let i = 0; i < 100; i++) {
-	let size = 30
-	let x = Math.random() * (canvas.width-size*2)+size
-	let y = Math.random() * (canvas.height-size*2)+size
-	let dx = (Math.random() - 0.5) * 5
-	let dy = (Math.random() - 0.5) * 5
+for(let i = 0; i < circlesAmount; i++) {
+	let x 		= Math.random() * (canvas.width-size*2)+size
+	let y 		= Math.random() * (canvas.height-size*2)+size
+	let dx 		= (Math.random() - 0.5) * speed
+	let dy 		= (Math.random() - 0.5) * speed
+	let rndSize = Math.random() * size + minRadius
 
-	circles.push(new Circle(x, y, dx, dy, size))
+	circles.push(new Circle(x, y, dx, dy, rndSize))
 }
 //console.log(circles)
 
